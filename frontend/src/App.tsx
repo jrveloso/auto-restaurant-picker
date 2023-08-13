3787dadda9550a35e5706f28ca2f865ce2e93f24
Jordan Veloso <jrveloso96@gmail.com>
2023-08-13 11:50:59 -0500

    Installed React Router Dom and React Router Bootstrap.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 function App() {
   return (
---
+++ frontend/src/pages/RestaurantPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RestaurantPage() {
+    return (
+        <h1>hi</h1>
+    )
+}
+
+export default RestaurantPage
